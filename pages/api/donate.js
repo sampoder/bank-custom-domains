1070d9579389d46e334f2e9a72d9beaf3d92f987
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-12-24 16:15:07 +0800

    Update donate.js

--- pages/api/donate.js
+++ pages/api/donate.js
@@ -4,29 +4,29 @@ var FormData = require("form-data");
 module.exports = async (req, res) => {
   const { host } = req.headers;
 
-  if(!req.body["donation[name]"]){
-    res.redirect('/')
+  if (!req.body["donation[name]"]) {
+    res.redirect("/");
+  } else {
+    var data = new FormData();
+    data.append("donation[name]", req.body["donation[name]"]);
+    data.append("donation[email]", req.body["donation[email]"]);
+    data.append("donation[amount]", req.body["donation[amount]"]);
+
+    var config = {
+      method: "post",
+      url: `https://bank.hackclub.com/donations/start/${process.env.username}`,
+      headers: {
+        ...data.getHeaders(),
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
   }
-
-  var data = new FormData();
-  data.append("donation[name]", req.body["donation[name]"]);
-  data.append("donation[email]", req.body["donation[email]"]);
-  data.append("donation[amount]", req.body["donation[amount]"]);
-
-  var config = {
-    method: "post",
-    url: `https://bank.hackclub.com/donations/start/${process.env.username}`,
-    headers: {
-      ...data.getHeaders(),
-    },
-    data: data,
-  };
-
-  axios(config)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
 };
